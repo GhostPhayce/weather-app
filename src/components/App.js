d4fad412d22288354d413bbcc14fa811e0cd60eb
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-10-03 16:12:12 +0100

    CSS styling added alongside Moment for dates and React Weather for icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+import "../styles/App.css";
 
 const App = ({ location, forecasts }) => (
-  <div className="forecast">
+  <div className="weather-app">
     <LocationDetails city={location.city} country={location.country} />
 
     <ForecastSummaries forecasts={forecasts} />
