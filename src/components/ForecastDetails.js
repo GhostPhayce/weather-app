c7160b16376da83e346633e753805fd0637c5921
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-10-06 22:03:58 +0100

    CSS styling applied to several sections. Wind and humidity needs to be pulled through

--- src/components/ForecastDetails.js
+++ src/components/ForecastDetails.js
@@ -1,20 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
+import "../styles/ForecastDetails.css";
 
 const ForecastDetails = ({ forecast }) => {
   const { date, temperature, description } = forecast;
 
   return (
-    <div className="forecast-summary">
-      <div className="forecast-summary__date">
+    <div className="forecast-details">
+      <div className="forecast-details__date">
         {moment(date).format("ddd Do MMM")}
       </div>
-      <div className="forecast-summary__temperature">
+      <div className="forecast-details__temperature">
         {temperature.max}
         &deg;C
       </div>
-      <div className="forecast-summary__description">{description}</div>
+      <div className="forecast-details__description">{description}</div>
     </div>
   );
 };
